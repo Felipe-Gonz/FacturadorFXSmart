9b0b6b28b7ecd0627be66cfc48f93c11e3a3f21c
Felipe-Gonz <fg180899@gmail.com>
2025-09-06 21:20:16 -0600

    Commit init

---
+++ src/main/java/com/example/FacturadorFXSmart/FacturadorFxSmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.FacturadorFXSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturadorFxSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturadorFxSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FacturadorFXSmart/FacturadorFxSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FacturadorFXSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturadorFxSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
